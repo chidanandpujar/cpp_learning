1b07e54e9f5b164dd51947df607ee63570290f01
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-22 17:26:50 +0530

    Create cppdecl.cpp
    b:1
    ch:A
    i:1007
    fl:4.87
    dl:12345.7

---
+++ cppdecl.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool b;
+	char ch;
+	int i;
+	float fl;
+	double dl;
+
+	b=true;
+	ch = 'A';
+	i = 1007;
+	fl = 4.87;
+	dl = 12345.6789;
+
+	cout << "b:" << b << "\n";
+	cout << "ch:" << ch << "\n";
+	cout << "i:" << i << "\n";
+	cout << "fl:" << fl << "\n";
+	cout << "dl:" << dl << "\n";
+	return 0;
+}
