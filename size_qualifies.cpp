ad54ff3e0996878dbe8a6b246766bf36397aa8cf
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-23 13:43:16 +0530

    Create size_qualifies.cpp
    size of short int: 2
    sizeo of long int: 8
    size of long double: 16

---
+++ size_qualifies.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        cout << "size of short int: " << sizeof(short int) << "\n";
+        cout << "sizeo of long int: " << sizeof(long int) << "\n";
+        cout << "size of long double: " << sizeof(long double) << "\n";
+        return 0;
+}
