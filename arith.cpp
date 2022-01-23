07780070cea75232276db95e609648094ae0358d
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-23 16:28:36 +0530

    Create arith.cpp

---
+++ arith.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=5 , b =10;
+	cout << b+a << "\n";
+	cout << b-a << "\n";
+	cout << b*a << "\n";
+	cout << b/a << "\n";
+	cout << b%a << "\n";
+	return 0;
+}
