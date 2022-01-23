2572e12da73e8f0e5f5f47a7e0084c067aaa48f5
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-23 13:28:59 +0530

    Create size.cpp
    size of bool: 1
    size of char: 1
    size of int: 4
    size of float: 4
    size of double: 8
    size of wchar: 4

---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	cout << "size of bool: " << sizeof(bool) << "\n";
+	cout << "size of char: " << sizeof(char) << "\n";
+	cout << "size of int: " << sizeof(int) << "\n";
+	cout << "size of float: " << sizeof(float) << "\n";
+	cout << "size of double: " << sizeof(double) << "\n";
+	cout << "size of wchar: " << sizeof(wchar_t) << "\n";
+	return 0;
+}
