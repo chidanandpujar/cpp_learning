b464d3aeedb8339b578d7894b155f0741749f24a
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-24 12:07:26 +0530

    Create remainder.cpp
    -3./.-2=1.5
    6./.7=8.57143
    -7./.6=-11.6667

---
+++ remainder.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        cout << "-3./.-2=" << -3./-2. << "\n";
+        cout << "6./.7=" << 6./.7 << "\n";
+        cout << "-7./.6=" << -7./.6 << "\n";
+        return 0;
+}
