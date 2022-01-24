9339153df0de2d21682cd3ff0118f283a273a49b
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-24 12:05:26 +0530

    Create divisions.cpp
    3.0f/2=-1.5
    -3/-3=:1
    -3/3=-1
    -3/1.4=-2.14286

---
+++ divisions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "3.0f/2=" << -3.0f/2 << "\n";
+	cout << "-3/-3=:" << -3/-3 << "\n";
+	cout << "-3/3=" << -3/3 << "\n";
+	cout << "-3/1.4=" << -3/1.4 << "\n";
+	return 0;
+}
