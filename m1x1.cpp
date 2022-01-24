a44d322ff98bce44795dffc0fa3987cde7cf194c
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-24 12:10:39 +0530

    Create m1x1.cpp
    d=35
    d=10

---
+++ m1x1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int a=10,b=5,c=5,d;
+        d=a+b*c;
+        cout << "d=" << d << "\n";
+        d=a/b*c;
+        cout << "d=" << d << "\n";
+        return 0;
+}
