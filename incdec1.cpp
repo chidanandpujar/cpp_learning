da7daa59ce6888fbd69a57202cf0731fb2e46062
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-24 13:00:43 +0530

    Create incdec1.cpp
    i=9
    i=10
    i=9
    i=8

---
+++ incdec1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int i=8;
+        ++i;
+        cout << "i=" << i << "\n";
+        i++;
+        cout << "i=" << i << "\n";
+        --i;
+        cout << "i=" << i << "\n";
+        i--;
+        cout << "i=" << i << "\n";
+        return 0;
+}
