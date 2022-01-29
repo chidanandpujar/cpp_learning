a13adf87892102504a7749ba617617df2d564d9a
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-01-29 18:08:51 +0530

    Create scopy.cpp

---
+++ scopy.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int i = 10;
+int main()
+{
+        int i = 20;
+        cout << "i=" << i << endl;
+        cout << "::i=" << ::i << endl;
+        return 0;
+}
