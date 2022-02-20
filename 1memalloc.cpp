d0c774ba59c4d26d6b5749c8e10beb9a6c426dfd
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-02-20 14:21:30 +0530

    Update 1memalloc.cpp
    ip vlaue = 77
    dp value =1.2
    ia value =10

--- 1memalloc.cpp
+++ 1memalloc.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 int main ()
 {
-        int *ip = new int (77);
-        cout << "ip vlaue = " << *ip << "\n";
-        return 0;
+	int *ip = new int (77);
+	cout << "ip vlaue = " << *ip << "\n";
+	double *dp = new double (1.2);
+	cout << "dp value =" << *dp << "\n";
+	int *ia = new int [10];
+	ia[0] = 10;
+	cout << "ia value =" << ia[0] << "\n";
+	return 0;
 }
-                 
