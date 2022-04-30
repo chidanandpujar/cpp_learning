93aa02100d4acd1d30974bbf2c45ed0ac01cb31e
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2022-04-30 17:25:13 +0530

    Create inhprivate.cpp
    Inheritance class with protected

---
+++ inhprivate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+class base {
+  private: int pvtb;
+  protected: int protb;
+  public:
+    viod pubb()
+    {
+      std::cout << "Pubb\n";
+    }
+};
+
+class dervived : protected base
+{
+  public:
+    void pubd()
+    {
+      std:cout << "pubd\n";
+    }
+ };
+
+int main()
+{
+  derived d1;
+  std::cout << d1.pvtb; //error
+  std::cout << d1.protb; //error
+  d1.pubb();  //ok runs
+  d1.pubd();  //ok runs
+  return 0;
+}
